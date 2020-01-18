baf62962db73abde2f7ba3dc601bfadf3fe6e2c2
Rodrigo Grisi <Rodricosta1995@gmail.com>
2020-01-18 20:37:33 -0300

    finalizando a segunda aula

---
+++ backend/src/index.js
@@ -0,0 +1,14 @@
+const express = require('express'); 
+const mongoose = require('mongoose')
+const routes = require('./routes')
+
+const app = express();
+
+mongoose.connect('mongodb+srv://oministack:oministack@cluster0-x4h2y.gcp.mongodb.net/week10?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+
+app.use(express.json())
+app.use(routes);
+app.listen(3333);
